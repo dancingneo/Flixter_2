22627d9b10ee7c8fecc6dc4cbe3cc1b6a1546456
yapagu <dreamerlim@gmail.com>
2021-09-12 18:55:40 -0700

    update

--- app/src/main/java/com/example/flixter/models/Movie.java
+++ app/src/main/java/com/example/flixter/models/Movie.java
@@ -35,7 +35,7 @@ public class Movie {
     }
 
     public String getBackdropPath() {
-        return String.format("https://image.tmdb.org/t/p/w342/$s" backdropPath);
+        return String.format("https://image.tmdb.org/t/p/w342/$s", backdropPath);
     }
 
     public String getTitle() {
